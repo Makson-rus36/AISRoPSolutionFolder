54d4ddc883431b94a6c53ae9b378841402afd785
Максим Тюкин <maksim.t8600@gmail.com>
2020-11-25 17:29:13 +0300

    верс от 25.11.2020 17:30

--- CMS/Core/DeleteData.cs
+++ CMS/Core/DeleteData.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace CMS.Core
 {
-    class DeleteData
+    public class DeleteData
     {
         public void DeleteDataOnDB(string tableName, string condition)
         {
-            string sql = String.Format("delete from {0} where {1}",tableName, condition);
-            InteractionsDB interactions = new InteractionsDB();
-            interactions.DbExecuteNoReturn(sql);
+            try
+            {
+                LoggerHelper.logger.startLog("Запрос на удаление принят: формирование");
+                string sql = String.Format("delete from {0} where {1}", tableName, condition);
+                LoggerHelper.logger.startLog("Запрос на удаление принят: исполнение: "+sql);
+                InteractionsDB interactions = new InteractionsDB();
+                interactions.DbExecuteNoReturn(sql);
+            }
+            catch (Exception exception)
+            {
+                LoggerHelper.logger.startLog(String.Format("Во время выполенения запроса произошла ошибка. \n" +
+                                                           "---------\n" +
+                                                           "Сообщение: {0}\n" +
+                                                           "Подробно: {1}\n" +
+                                                           "Трассировка стека: {2}\n" +
+                                                           "---------", exception.Message, exception.InnerException, exception.StackTrace));
+            }
         }
     }
 }
